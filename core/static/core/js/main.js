87768ca5a412dab82c6bde8c37e4eb99fa95ae80
Lua-Code12 <60054359+Lua-Code12@users.noreply.github.com>
2020-07-06 17:21:00 -0400

    faltante

---
+++ core/static/core/js/main.js
@@ -0,0 +1,51 @@
+/**Primer slider**/
+$(".slider-one")
+    .not(".slick-intialized")
+    .slick({
+    autoplay:true,
+    autoplaySpeed: 3000,
+    dots:true,
+    preArrow: ".site-slider .slider-btn .prev",
+    nextArrow: ".site-slider .slider-btn .next",
+});
+
+
+/**Segundo slider**/
+
+$(".slider-two")
+    .not(".slick-intialized")
+    .slick({
+    preArrow: ".site-slider-two .prev",
+    nextArrow: ".site-slider-two .next",
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000
+});
+
+
+/**tercer slider**/
+
+$(".slider-three")
+    .not(".slick-intialized")
+    .slick({
+    preArrow: ".site-slider-three .prev",
+    nextArrow: ".site-slider-three .next",
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000,
+    infinite: false
+});
+
+
+/**Cuarto slider**/
+
+$(".slider-four")
+    .not(".slick-intialized")
+    .slick({
+    preArrow: ".site-slider-four .prev",
+    nextArrow: ".site-slider-four .next",
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000,
+    infinite: false
+});
